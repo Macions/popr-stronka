b33ddd1a7e0820c408f439f47a81a5b3bab8d3dc
Maciej Czarnecki <maciejkaczarnecka@gmail.com>
2024-06-01 20:08:43 +0200

    v7

---
+++ popr/script.js
@@ -0,0 +1,11 @@
+document.querySelector('.school').addEventListener('click', function() {
+    window.location.href = 'school.html';
+});
+
+document.querySelector('.home').addEventListener('click', function() {
+    window.location.href = 'home.html';
+});
+
+document.querySelector('.exercises').addEventListener('click', function() {
+    window.location.href = 'exercises.html';
+});
